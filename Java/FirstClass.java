9cc38cd099698f7f860ce4a8511fed087e1f6380
Deep2306patel <103233788+Deep2306patel@users.noreply.github.com>
2023-03-08 17:50:52 +0530

    first commit

---
+++ Java/FirstClass.java
@@ -0,0 +1,16 @@
+class FirstClass{
+    public static void main(String args[]) {
+        System.out.println("hello");
+    }
+}
+
+// // 1 function i.e. void main(){
+
+// } 
+
+// 2 class
+    // class main {
+    //     void main(){
+
+    //     }
+    // }
