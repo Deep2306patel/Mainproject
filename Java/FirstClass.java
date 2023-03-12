c1da6964355ee96dae0de08a1dbb42c0e56ba198
Deep2306patel <103233788+Deep2306patel@users.noreply.github.com>
2023-03-12 10:43:37 +0530

    #

--- Java/FirstClass.java
+++ Java/FirstClass.java
@@ -1,16 +1,19 @@
+import java.util.*;
+
 class FirstClass{
     public static void main(String args[]) {
+        // system.out.print("pass string or output"); //// <--------- output
         System.out.println("hello");
+        System.out.print("world");
+        // variables
+        String name = "world";
+        int a = 21;
+        System.out.print(name);
+        System.out.print(a);
+        // input
+        Scanner sc = new Scanner(System.in);
+        String name2 = sc.nextLine();
+        System.out.println(name2);
     }
 }
 
-// // 1 function i.e. void main(){
-
-// } 
-
-// 2 class
-    // class main {
-    //     void main(){
-
-    //     }
-    // }
